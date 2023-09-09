eaa2699e160011679d8abd186d173574b5e86352
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-09-09 18:43:37 +0530

    updated servel modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ThemeProvider } from "@/components/providers/themeProvider";
+import { ModalProvider } from "@/components/providers/modalProvider";
 import { cn } from "@/lib/utils";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
             enableSystem
             storageKey="distalk-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
