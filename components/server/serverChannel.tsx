c22e7375b6b46413f31f7731fe72a9e75e9ba657
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-09-12 16:56:49 +0530

    added channel id page

--- components/server/serverChannel.tsx
+++ components/server/serverChannel.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { useModal } from "@/hooks/useModalStore";
+import { useModal, ModalType } from "@/hooks/useModalStore";
 import { ActionTooltip } from "@/components/actionTooltip";
 
 interface ServerChannelProps {
@@ -25,10 +25,18 @@ export const ServerChannel = ({
 }: ServerChannelProps) => {
   const { onOpen } = useModal();
   const params = useParams();
+  const router = useRouter();
   const Icon = iconMap[channel.type];
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { channel, server });
+  };
   return (
     <button
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -49,13 +57,13 @@ export const ServerChannel = ({
           <ActionTooltip label="Edit">
             <Edit
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-              onClick={() => onOpen("editChannel", { server, channel })}
+              onClick={(e) => onAction(e, "editChannel")}
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-              onClick={() => onOpen("deleteChannel", { server, channel })}
+              onClick={(e) => onAction(e, "deleteChannel")}
             />
           </ActionTooltip>
         </div>
--- components/server/serverMember.tsx
+++ components/server/serverMember.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Member, MemberRole, Profile, Server } from "@prisma/client";
 import { ShieldAlert, ShieldCheck } from "lucide-react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/userAvatar";
 
@@ -18,9 +18,14 @@ const roleIconMap = {
 };
 export const ServerMember = ({ member, server }: ServerMemberProps) => {
   const params = useParams();
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
   const icon = roleIconMap[member.role];
   return (
     <button
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -33,7 +38,7 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
       <p
         className={cn(
           "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-          params?.channelId === member.id &&
+          params?.memberId === member.id &&
             "text-primary dark:text-zinc-200 dark:group-hover:text-white"
         )}
       >
