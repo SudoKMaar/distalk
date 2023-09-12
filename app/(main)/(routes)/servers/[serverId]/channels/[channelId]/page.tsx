0f32bc1c82550fb92db1a16f6e70407104f92b06
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-09-12 19:19:20 +0530

    added chatbox

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -2,6 +2,7 @@ import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/currentProfile";
 import { ChatHeader } from "@/components/chat/chatHeader";
+import { ChatInput } from "@/components/chat/chatInput";
 import { db } from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -36,6 +37,16 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
