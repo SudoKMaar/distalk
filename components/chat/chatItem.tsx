35d24759084a2c98382e9e4ee7ee38acfa5c5b2e
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-09-16 16:06:46 +0530

    added delete message feature

--- components/chat/chatItem.tsx
+++ components/chat/chatItem.tsx
@@ -11,7 +11,6 @@ import {
   FileQuestion,
   FileText,
   FileVideo2,
-  FileX,
   ShieldAlert,
   ShieldCheck,
   Trash,
@@ -24,6 +23,7 @@ import { cn } from "@/lib/utils";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useModal } from "@/hooks/useModalStore";
 
 interface ChatItemProps {
   id: string;
@@ -63,7 +63,7 @@ export const ChatItem = ({
   socketQuery,
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
+  const { onOpen } = useModal();
   useEffect(() => {
     const handleKeyDown = (event: any) => {
       if (event.key === "Escape" || event.keyCode === 27) {
@@ -350,7 +350,15 @@ export const ChatItem = ({
             </ActionTooltip>
           )}
           <ActionTooltip label="Delete">
-            <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+            <Trash
+              className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+              onClick={() =>
+                onOpen("deleteMessage", {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
+            />
           </ActionTooltip>
         </div>
       )}
--- components/providers/modalProvider.tsx
+++ components/providers/modalProvider.tsx
@@ -11,6 +11,7 @@ import { DeleteServerModal } from "@/components/modals/deleteServerModal";
 import { DeleteChannelModal } from "@/components/modals/deleteChannelModal";
 import { EditChannelModal } from "@/components/modals/editChannelModal";
 import { MessageFileModal } from "@/components/modals/messageFileModal";
+import { DeleteMessageModal } from "@/components/modals/deleteMessageModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -33,6 +34,7 @@ export const ModalProvider = () => {
       <DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
+      <DeleteMessageModal />
     </>
   );
 };
